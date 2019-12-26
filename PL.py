a8058ca9686d2db2841de5f5ad7e1e9eb63243bc
hoangdungnguyen <31024849+hoangdungnguyen@users.noreply.github.com>
2019-12-26 16:27:12 +0100

    dropoutNN+PL.py

--- PL.py
+++ PL.py
@@ -1,26 +1,42 @@
-# This code runs parallelly a Neural Network with and without Pseudolabeling
-
 import os
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+import time
+import tensorflow as tf
+import tensorflow as tfp
 import numpy as np
+import pandas as pd
+
 import matplotlib.pyplot as plt
 
 from tensorflow.examples.tutorials.mnist import input_data
+from sklearn.model_selection import StratifiedShuffleSplit
 
 mnist = input_data.read_data_sets("", one_hot=True)
 
+stratSplit = StratifiedShuffleSplit(test_size=100, random_state=2812, n_splits=1)
+stratSplit.get_n_splits(mnist.train.images, np.argmax(mnist.train.labels, axis = 1))
+for train_index, test_index in stratSplit.split(X = mnist.train.images,y = mnist.train.labels):
+  x_train = mnist.train.images[test_index]
+  y_train = mnist.train.labels[test_index]
+  x_PL = mnist.train.images[train_index]
+
+print('Labeled data size :', x_train.shape)
+print('Labeled data size :', x_PL.shape)
+print('Proportion of class label in train data: ')
+pd.DataFrame(np.unique(np.argmax(y_train,1), return_counts = True))
+
+tf.test.is_gpu_available()
+
 # Neural Network parameters
-iteration_list = np.zeros(1)
-neural_network_accuracy_list = np.zeros(1)
-pseudo_label_accuarcy_list = np.zeros(1)
+iteration_list = []
+neural_network_accuracy_list = []
+pseudo_label_accuarcy_list = []
 neural_network_accuracy = 0
 pseudo_label_accuarcy = 0
 
 learningRate = 1.5
-trainingEpochs = 500
+trainingEpochs = 3000
+dropoutRate_0 = 0.2
+dropoutRate_1 = 0.5
 
 inputN = 784
 hiddenN = 5000
@@ -31,67 +47,75 @@ PLbatchSize = 256
 
 iteration = 0
 cPL = 0
+
 T1 = 100
 T2 = 600
 a = 0.
 af = 3.
 
-print("HiddenLayer1:", hiddenN)
+T = 500
+k = 0.998
+pi = 0.5
+pf = 0.99
 
 x = tf.placeholder("float", [None, inputN])
 y = tf.placeholder("float", [None, outputN])
 PLx = tf.placeholder("float", [None, inputN])
 PLy = tf.placeholder("float", [None, outputN])
-alpha = tf.placeholder("float", )
-
+alpha_t = tf.placeholder("float", )
+p_t = tf.placeholder("float",)
+epsilon_t = tf.placeholder("float",)
 plt.clf()
-
 
 def NN(x, w, b):
     # Hidden layer 1
     HL = tf.add(tf.matmul(x, w['h1']), b['b1'])
-    HL = tf.nn.sigmoid(HL)  # sigmoid(HL)
-
+    HL = tf.nn.relu(HL)
+    HL = tf.nn.dropout(HL, rate = dropoutRate_1)
     # Output layer
-    out_layer = tf.matmul(HL, w['out']) + b['out']
+    out_layer =tf.matmul(HL, w['out']) + b['out']
 
     return out_layer
 
-
 # initialize weights and biases
+with tf.variable_scope('NN') :
+  weightsNN = {
+    'h1': tf.Variable(tf.random_normal([inputN, hiddenN])),
+    'out': tf.Variable(tf.random_normal([hiddenN, outputN]))
+    }
 
-weightsNN = {
-    'h1': tf.Variable(tf.random_normal([inputN, hiddenN])),
-    'out': tf.Variable(tf.random_normal([hiddenN, outputN]))
-}
-biasesNN = {
+  biasesNN = {
     'b1': tf.Variable(tf.random_normal([hiddenN])),
     'out': tf.Variable(tf.random_normal([outputN]))
-}
-weightsPL = {
+    }
+
+with tf.variable_scope('PL') :
+  weightsPL = {
     'h1': tf.Variable(tf.random_normal([inputN, hiddenN])),
     'out': tf.Variable(tf.random_normal([hiddenN, outputN]))
-}
-biasesPL = {
+    }
+  biasesPL = {
     'b1': tf.Variable(tf.random_normal([hiddenN])),
     'out': tf.Variable(tf.random_normal([outputN]))
-}
+    }
 
 predNN = NN(x, weightsNN, biasesNN)
 predPL = NN(x, weightsPL, biasesPL)
 predPL1 = NN(PLx, weightsPL, biasesPL)
 
-costNN = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=predNN,
+costNN = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=predNN,
                                                                 labels=y))
 
-costPL = tf.add(tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=predPL,
+costPL = tf.add(tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=predPL,
                                                                        labels=y)),
-                (alpha * tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=predPL1,
+                (alpha_t * tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=predPL1,
                                                                                 labels=PLy))))
 
 # Gradient Descent
-optimizerNN = tf.train.GradientDescentOptimizer(learningRate).minimize(costNN)
-optimizerPL = tf.train.GradientDescentOptimizer(learningRate).minimize(costPL)
+optimizerNN = tf.train.MomentumOptimizer(learning_rate = (1-p_t)*epsilon_t,
+                                        momentum = -p_t/(1-p_t)).minimize(costNN)
+optimizerPL = tf.train.MomentumOptimizer(learning_rate = (1-p_t)*epsilon_t,
+                                        momentum = -p_t/(1-p_t)).minimize(costNN)
 
 # Initializing the variables
 init = tf.global_variables_initializer()
@@ -118,29 +142,46 @@ def accuracytestPL():
 
 with tf.Session() as sess:
     sess.run(init)
-
+    begin=time.time()
     # Training cycle
     for epoch in range(trainingEpochs):
         avg_costNN = 0.
         avg_costPL = 0.
-        total_batch = int(3000 / batchSize)
+        total_batch = int(100 / batchSize)
         # Loop over all batches
+        index = np.arange(x_train.shape[0])
+        np.random.shuffle(index)
+        batches_X = np.array_split(x_train[index], total_batch)
+        batches_y = np.array_split(y_train[index], total_batch)
+        index = np.arange(x_PL.shape[0])
+        np.random.shuffle(index)
+        batches_X_PL = np.array_split(x_PL[index], total_batch)
         for i in range(total_batch):
-            batch_x, batch_y = mnist.train.next_batch(batchSize)
+            batch_x, batch_y = batches_X[i], batches_y[i]
+            if epoch > T:
+              p = pf
+            else:
+              p = epoch/T*pf + (1-epoch/T)*pi
 
             _, cNN = sess.run([optimizerNN, costNN], feed_dict={x: batch_x,
-                                                                y: batch_y})
-            if iteration > T1:
-                a = ((iteration - T1) / (T2 - T1)) * af
-                if iteration > T2:
-                    a = af
+                                                                y: batch_y,
+                                                                p_t : p,
+                                                                epsilon_t : learningRate*(k**epoch)
+                                                                })
+            if epoch < T1:
+                a = 0
+            elif epoch < T2:
+                a = ((epoch - T1) / (T2 - T1)) * af
+            else :
+                a = af
+            
 
             # Pseudolabel
-            batch_xpred, _ = mnist.train.next_batch(PLbatchSize)
+            batch_xpred = batches_X_PL[i]
             batch_ypred = sess.run([predPL], feed_dict={x: batch_xpred})
             batch_ypred = batch_ypred[0]
             batch_ypred = batch_ypred.argmax(1)
-            batch_ypre = np.zeros((PLbatchSize, 10))
+            batch_ypre = np.zeros((batch_xpred.shape[0], 10))
             for ii in range(PLbatchSize):
                 batch_ypre[ii, batch_ypred[ii]] = 1
 
@@ -148,16 +189,20 @@ with tf.Session() as sess:
                                                                 y: batch_y,
                                                                 PLx: batch_xpred,
                                                                 PLy: batch_ypre,
-                                                                alpha: a})
-            iteration = iteration + 1
+                                                                p_t : p,
+                                                                epsilon_t : learningRate*(k**epoch),
+                                                                alpha_t: a})
+            iteration += 1
             # Compute average loss
             avg_costNN += cNN / total_batch
             avg_costPL += cPL / total_batch
 
-        if iteration % 100 == 0:
-            print('t=', iteration)
+        if epoch % 100 == 0:
             neural_network_accuracy = accuracytestNN()
             pseudo_label_accuarcy = accuracytestPL()
+            print("Epoch {} | time = {:.4f} | DropNN acc = {:.4f} | DropNN+PL acc = {:.4f} "
+                  .format(epoch, time.time() - begin,
+                          neural_network_accuracy, pseudo_label_accuarcy))
 
             iteration_list = np.append(iteration_list, iteration)
             neural_network_accuracy_list = np.append(neural_network_accuracy_list, neural_network_accuracy)
